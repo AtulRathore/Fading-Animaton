f6ef6da72e3b3a20cf654f704b796066013a403d
Atul Rathore <kshtriya.atul@gmail.com>
2017-01-05 00:17:13 +0530

    C2

--- app/src/main/java/com/example/atul/udemy_animation/MainActivity.java
+++ app/src/main/java/com/example/atul/udemy_animation/MainActivity.java
@@ -10,8 +10,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void fade(View view){
 
+        //int count=0;
         ImageView bart = (ImageView) findViewById(R.id.imageView);
-        bart.animate().alpha(0).setDuration(2000);
+        ImageView homer = (ImageView) findViewById(R.id.imageView2);
+
+        bart.animate().alpha(0f).setDuration(2000);
+        homer.animate().alpha(1f).setDuration(2000);
+        /*while (count<20){
+            if(count%2==0){
+                bart.animate().alpha(1f).setDuration(2000);
+                homer.animate().alpha(0f).setDuration(2000);
+                count++;
+            }else{
+                bart.animate().alpha(0f).setDuration(2000);
+                homer.animate().alpha(1f).setDuration(2000);
+                count++;
+            }
+        }*/
     }
 
     @Override
